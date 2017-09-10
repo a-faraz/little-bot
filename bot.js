b2d8d9b7a33dea2491aff48a37e5ec2d59b6aff0
a-faraz <anjumfaraz10@gmail.com>
2017-09-10 00:39:35 -0500

    changes to bot

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Twit = require('twit');
+const config = require('./config');
+const txtgen = require('txtgen');
+const T = new Twit(config);
+
+setInterval(() => {
+  
+  // Interval between hour 10 and 11
+  const hour = new Date().getHours();
+  if (hour >= 10 && hour < 11) {
+    justTweetIt();
+  }
+} , 1000*60*60);
+
+
+function justTweetIt () {
+
+  // Generating the random sentence
+  let sentence = txtgen.sentence();
+
+  // Posting the tweet
+  T.post('statuses/update', sentence, (err, data, response) => {
+    if (err) {
+      console.log('Error: ', err);
+    } else {
+      console.log('Data: ', data);
+    }
+  });
+
+};
+
